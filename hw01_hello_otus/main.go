2999a45dfd3911881a20354f6fdee2783ceb7450
EvGesh4And <zheka511andreev@gmail.com>
2025-02-02 00:32:09 +0300

    hw01 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
+// Пакет main демонстрирует использование пакета reverse для инвертирования строки
 package main
 
+import (
+	"fmt" // Импортируем встроенный пакет fmt для форматированного ввода/вывода
+
+	"golang.org/x/example/hello/reverse" // Импортируем пакет reverse для работы с инверсией строк
+)
+
 func main() {
-	// Place your code here.
+	s := "Hello, OTUS!"            // Исходная строка, которую нужно перевернуть
+	fmt.Println(reverse.String(s)) // Выводим результат реверса строки в консоль
 }
