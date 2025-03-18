7819ff6bf53e8325ebc783cd4eecb4a57f9364d3
EvGesh4And <zheka511andreev@gmail.com>
2025-03-18 23:50:08 +0300

    HW5 in work

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,48 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	// Канал для отправки сигнала завершения работы (для генератора и воркеров)
+	done := make(chan struct{})
+	// Канал для остановки всех горутин (буфер n, чтобы не было блокировок)
+	stop := make(chan struct{}, n)
+
+	// Создаём объект для управления остановкой со слежением за лимитом ошибок
+	stopper := NewStopper(stop, m)
+
+	// Ожидание завершения генератора и всех воркеров
+	var wg sync.WaitGroup
+
+	// Горутину для отправки сигнала завершения работы после получения сигнала из stop
+	go func() {
+		defer close(done) // Закрываем канал done по завершению работы
+		<-stop            // Ожидаем получения первого сигнала из stop
+	}()
+
+	// Генерация задач и отправка их в канал taskStream
+	wg.Add(1)
+	taskStream := generator(done, tasks, n, &wg)
+
+	// Запуск n воркеров
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go worker(done, taskStream, stopper, &wg)
+	}
+
+	// Ожидаем завершения всех воркеров
+	wg.Wait()
+
+	// Если лимит ошибок превышен, возвращаем ошибку
+	if stopper.StatusOverLimit {
+		return ErrErrorsLimitExceeded
+	}
+	// Если ошибок не было, возвращаем nil
 	return nil
 }
