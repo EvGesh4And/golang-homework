bdd41324641d81cf383b0ca20661bb0f594fbe34
EvGesh4And <zheka511andreev@gamil.com>
2025-05-06 18:51:26 +0300

    hw11_telnet_client

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,42 @@
 package main
 
+import (
+	"log"
+	"net"
+	"os"
+	"time"
+
+	"github.com/spf13/pflag"
+)
+
 func main() {
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := pflag.Duration("timeout", 10*time.Second, "timeout for connecting to the server")
+	pflag.Usage = func() {
+		log.Print("Usage: go-telnet [--timeout duration] <host> <port>")
+		pflag.PrintDefaults()
+	}
+	pflag.Parse()
+
+	// Проверяем наличие двух позиционных аргументов: host и port
+	args := pflag.Args()
+	if len(args) < 2 {
+		log.Fatal("Not all operands are specified")
+	}
+	host := args[0]
+	port := args[1]
+	addr, err := net.ResolveTCPAddr("tcp", host+":"+port)
+	if err != nil {
+		log.Fatalf("Incorrect address: %v", err)
+	}
+
+	telClient := NewTelnetClient(addr.String(), *timeout, os.Stdin, os.Stdout)
+	log.Print(addr.String())
+	err = telClient.Connect()
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	go telClient.Send()
+	go telClient.Receive()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"io"
+	"log"
+	"net"
 	"time"
 )
 
@@ -13,9 +17,72 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &MyTelnetClinet{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type MyTelnetClinet struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+	ctx     context.Context
+}
+
+func (mtc *MyTelnetClinet) Connect() error {
+	ctx, _ := context.WithTimeout(context.Background(), mtc.timeout)
+	// defer cancel()
+
+	dial := net.Dialer{}
+	conn, err := dial.DialContext(ctx, "tcp", mtc.address)
+	if err != nil {
+		return err
+	}
+	// defer conn.Close()
+	log.Printf("Connected to %s", mtc.address)
+	mtc.conn = conn
+	mtc.ctx = ctx
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (mtc *MyTelnetClinet) Close() error {
+	if mtc.conn != nil {
+		err := mtc.conn.Close()
+		if err != nil {
+			return err
+		}
+		err = mtc.in.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (mtc *MyTelnetClinet) Send() error {
+	scanner := bufio.NewScanner(mtc.in)
+
+	for scanner.Scan() {
+		_, err := mtc.conn.Write([]byte(scanner.Text() + "\n"))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (mtc *MyTelnetClinet) Receive() error {
+	scanner := bufio.NewScanner(mtc.conn)
+	for scanner.Scan() {
+		_, err := mtc.out.Write([]byte(scanner.Text() + "\n"))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
