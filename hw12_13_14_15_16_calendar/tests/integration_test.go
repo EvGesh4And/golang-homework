03f48c1daffa45706e4151a26e3bd8ec786659d3
EvGesh4And <zheka511andreev@gmail.com>
2025-06-23 13:19:59 +0300

    hw15_calendar int_test5

--- hw12_13_14_15_16_calendar/tests/integration_test.go
+++ hw12_13_14_15_16_calendar/tests/integration_test.go
@@ -16,26 +16,51 @@ import (
 )
 
 var _ = Describe("POST /event", func() {
-	It("creates an event successfully", func() {
-		event := storage.Event{
-			ID:          uuid.New(),
-			Title:       "test event",
-			Description: "test desc",
-			UserID:      uuid.New(),
-			Start:       time.Now().Add(time.Hour),
-			End:         time.Now().Add(2 * time.Hour),
-			TimeBefore:  10 * time.Minute,
-		}
+	Context("create event", func() {
+		It("creates an event successfully", func() {
+			event := storage.Event{
+				ID:          uuid.New(),
+				Title:       "test event",
+				Description: "test desc",
+				UserID:      uuid.New(),
+				Start:       time.Now().Add(time.Hour),
+				End:         time.Now().Add(2 * time.Hour),
+				TimeBefore:  10 * time.Minute,
+			}
 
-		eventDTO := storage.ToDTO(event)
+			eventDTO := storage.ToDTO(event)
 
-		body, err := json.Marshal(eventDTO)
-		Expect(err).To(BeNil())
+			body, err := json.Marshal(eventDTO)
+			Expect(err).To(BeNil())
 
-		resp, err := http.Post("http://localhost:8888/event", "application/json", bytes.NewReader(body))
-		Expect(err).To(BeNil())
-		defer resp.Body.Close()
+			resp, err := http.Post("http://localhost:8888/event", "application/json", bytes.NewReader(body))
+			Expect(err).To(BeNil())
+			defer resp.Body.Close()
 
-		Expect(resp.StatusCode).To(Equal(http.StatusCreated))
+			Expect(resp.StatusCode).To(Equal(http.StatusCreated))
+		})
+
+		It("creates an event invalid event", func() {
+			invalidEvent := storage.Event{
+				ID:          "trash",
+				Title:       "",
+				Description: "test desc",
+				UserID:      uuid.New(),
+				Start:       time.Now().Add(time.Hour),
+				End:         time.Now().Add(2 * time.Hour),
+				TimeBefore:  10 * time.Minute,
+			}
+
+			eventDTO := storage.ToDTO(invalidEvent)
+
+			body, err := json.Marshal(eventDTO)
+			Expect(err).To(BeNil())
+
+			resp, err := http.Post("http://localhost:8888/event", "application/json", bytes.NewReader(body))
+			Expect(err).To(BeNil())
+			defer resp.Body.Close()
+
+			Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
+		})
 	})
 })
